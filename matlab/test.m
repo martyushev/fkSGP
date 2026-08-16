082b8738e049fcfc8fd067470a8d54a8869eca3f
martyushev <72862703+martyushev@users.noreply.github.com>
2026-08-16 18:11:29 +0500

    motion type added

--- matlab/test.m
+++ matlab/test.m
@@ -1,19 +1,24 @@
 clear; clc;
 
 sgpType = 1;
-% 1:   general 6-6 SGP
-% 2:   semi-planar 6P-6 SGP
-% 3:   fully planar 6P-6P SGP
-% 4:   general 6-5 SGP
-% 5:   semi-planar 6P-5 SGP
-% 6:   fully planar 6P-5P SGP
-% 100: general 6-6 SGP with 40 real postures (by P. Dietmaier)
+% 1:   general 6-6 (|^6) SGP
+% 2:   semi-planar 6P-6 (|^6) SGP
+% 3:   fully planar 6P-6P (|^6) SGP
+% 4:   general 6-5 (/\ |^4) SGP
+% 5:   semi-planar 6P-5 (/\ |^4) SGP
+% 6:   fully planar 6P-5P (/\ |^4) SGP
+% 100: general 6-6 (|^6) SGP with 40 real postures (by P. Dietmaier)
+
+motionType = 1;
+% 1:   generic motion
+% 2:   pure translation
+% 3:   pure z-translation
 
 realOnly = 1;
 % 0: return all complex postures
 % 1: return real postures only
 
-sgp = genSGP(sgpType);
+sgp = genSGP(sgpType,motionType);
 
 tic;
 [Rt,err] = feval(sgp.solver,sgp.X,sgp.x,sgp.L,realOnly);
